b8c09ad23775aff96a4ed261d7f73d02796c1c91
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-02-16 11:05:45 +0700

    @@

---
+++ Bai2/Thuchanh1/src/main/java/com/quanganhdo/Service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.quanganhdo.Service;
+
+import com.quanganhdo.Model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
