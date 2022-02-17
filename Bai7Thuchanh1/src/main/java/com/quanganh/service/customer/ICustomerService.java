618bf2e4621f998dd3d9ba05717baa7fb5a492fd
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-02-17 18:25:59 +0700

    @@

--- Bai7Thuchanh1/src/main/java/com/quanganh/service/customer/ICustomerService.java
+++ Bai7Thuchanh1/src/main/java/com/quanganh/service/customer/ICustomerService.java
@@ -3,7 +3,11 @@ package com.quanganh.service.customer;
 import com.quanganh.model.Customer;
 import com.quanganh.model.Province;
 import com.quanganh.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ICustomerService extends IGeneralService<Customer> {
     Iterable<Customer> findAllByProvince(Province province);
+    Page<Customer> findAll(Pageable pageable);
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
 }
