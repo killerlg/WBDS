536122e7ccc74ae0d9d36e430b61cfcb3ce9b821
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-02-24 13:58:50 +0700

    @@

---
+++ customer-management/src/main/java/quanganh/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package quanganh.repository;
+
+import quanganh.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ customer-management/src/main/java/quanganh/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package quanganh.service;
+
+import quanganh.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
