de56ddc626507a36c30b4cf020f875ae477df821
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-02-28 19:00:33 +0700

    @@

---
+++ DemoApp/src/main/java/com/quanganh/demoapp/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package com.quanganh.demoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public ModelAndView showGreeting(){
+        return new ModelAndView("/greeting");
+    }
+}
